a6aac675497c6eb75f391501dba71b83ce4c16bd
Tom78200 <teratouille@outlook.fr>
2025-12-04 21:40:19 +0100

    Chore: Trigger Vercel redeploy

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,5 @@
 import express, { type Request, Response, NextFunction } from "express";
+// Force Vercel redeploy
 import dotenv from "dotenv";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
